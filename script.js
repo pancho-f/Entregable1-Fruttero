e1d275fb939913e6f68a4f16918ae46b26d3308e
pancho-f <116600235+pancho-f@users.noreply.github.com>
2024-07-04 16:39:19 -0300

    entregable 1 listo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// se declara el array para la orden y los arrays para los productos
+// se declara el array para la orden y los arrays para los productos disponibles
 let orden1 = []
 const productosComida = ["Pizza", "Hamburguesa", "Barra de Chocolate"]
 const productosIndumentaria = ["Gorra", "Lentes de sol", "Camisa"]
@@ -8,68 +8,64 @@ const productosElectro = ["Microondas", "Televisor", "Dron"]
 const compraComida = () => {
     let productoElegido = parseInt(prompt("Elegí el producto a agregar: \n 1. Pizza \n 2. Hamburguesa \n 3. Barra de Chocolate"))
     productoElegido -=1
-    orden1.push(productosComida[productoElegido])
+    if(productoElegido<=(productosComida.length -1) && productoElegido>-1){
+        orden1.push(productosComida[productoElegido])
+    }else {alert("Ingresaste un valor inválido, intenta nuevamente.")}
 }
 const compraIndumentaria = () => {
     let productoElegido = parseInt(prompt("Elegí el producto a agregar: \n 1. Gorra \n 2. Lentes de sol \n 3. Camisa"))
     productoElegido -=1
-    orden1.push(productosIndumentaria[productoElegido])
+    if(productoElegido<=(productosIndumentaria.length -1) && productoElegido>-1){
+        orden1.push(productosIndumentaria[productoElegido])
+    }else {alert("Ingresaste un valor inválido, intenta nuevamente.")}
 }
 const compraElectro = () => {
     let productoElegido = parseInt(prompt("Elegí el producto a agregar: \n 1. Microondas \n 2. Televisor \n 3. Dron"))
     productoElegido -=1
-    orden1.push(productosElectro[productoElegido])
+    if(productoElegido<=(productosElectro.length -1) && productoElegido>-1){
+        orden1.push(productosElectro[productoElegido])
+    }else {alert("Ingresaste un valor inválido, intenta nuevamente.")}
+}
+
+//funcion para eliminar productos, uno a la vez
+const editarCompra = () => {
+    let productoElegido = prompt("Elegí el producto a eliminar: \n"+orden1.join("\n"))
+    if (orden1.includes(productoElegido)){
+        productoElegido = orden1.indexOf(productoElegido)
+        orden1.splice(productoElegido, 1)
+    }else {alert("Ingresaste un valor inválido, intenta nuevamente.")}
 }
 
 
 // interfaz de compra
-let categoriaElegida = prompt("Elegí la categoría: \n 1. Comida \n 2. Indumentaria \n 3. Electrodomésticos \n 4. Muebles \n 5. Varios \n 6.Cancelar")
-while (categoriaElegida !== "6"){
-    switch(categoriaElegida){
-        case categoriaElegida="1": compraComida()
-            break;
-        case categoriaElegida="2": compraIndumentaria()
-            break;
-        case categoriaElegida="3": compraElectro()
-            break;
-        default: console.log("opcion invalida")
+const interfazCompra = () => {
+    let categoriaElegida = prompt("Elegí la categoría: \n 1. Comida \n 2. Indumentaria \n 3. Electrodomésticos \n 5. Editar orden \n 6. Cancelar")
+    while (categoriaElegida !== true){
+        while (categoriaElegida !== "6"){
+            switch(categoriaElegida){
+                case categoriaElegida="1": compraComida()
+                    break;
+                case categoriaElegida="2": compraIndumentaria()
+                    break;
+                case categoriaElegida="3": compraElectro()
+                    break;
+                case categoriaElegida="5": editarCompra()
+                    break;
+                case categoriaElegida=false:
+                    break;
+                default: alert("Opcion inválida, intenta nuevamente.")
+            }
+            categoriaElegida = prompt("Elegí la categoría: \n 1. Comida \n 2. Indumentaria \n 3. Electrodomésticos \n 5. Editar orden \n 6. Terminar")
+        } categoriaElegida = confirm("Estás seguro?")
     }
-    categoriaElegida = prompt("Elegí la categoría: \n 1. Comida \n 2. Indumentaria \n 3. Electrodomésticos \n 6.Terminar")
 }
 
-// envia el mensaje final sobre la orden
+//llama a la funcion
+interfazCompra()
+
+// envia el mensaje final sobre la orden.   posiblemente se deberia incorporar a la funcion interfazCompra ? y tambien podria ser en forma de alerta en lugar de console.log
 if(orden1.length!==0){
-    console.log("la orden de compra de "+orden1+" se ha enviado")
+    console.log("La orden de compra de los siguientes productos: \n-"+orden1.join(".\n-")+".\nSe ha enviado.")
 }else{
-    console.log("la compra se ha cancelado")
+    console.log("La compra se ha cancelado.")
 }
-
-console.log("test")             //no hace nada
-
-
-
-//funcion flecha ejemplo
-const funcionNombre = parametro => parametro*10
-console.log(funcionNombre(10))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
